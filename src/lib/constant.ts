3aa079c940f709d5164c44d0d90f50acfc35e1c0
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2020-07-28 18:26:30 +0700

    Add latest announce date in Dynamo for check message duplication (#3)

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,3 +1,4 @@
 export const LINE_NOTIFY_ACCESS_TOKEN = process.env.LINE_NOTIFY_ACCESS_TOKEN;
 export const LINE_NOTIFY_URL = process.env.LINE_NOTIFY_URL;
 export const WEATHER_WARNING_QUEUE_NAME = 'weather-warning-line-notification-queue';
+export const DYNAMO_DB_TABLE_NAME = process.env.DYNAMO_DB_TABLE_NAME || '';
